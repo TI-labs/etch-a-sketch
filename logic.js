99e37c2671bda5c0e942436bee429fcccc247a29
Tidjani Stefel <tidjani.stefel@outlook.com>
2022-08-02 13:27:59 +0200

    crée l'effet hover

--- logic.js
+++ logic.js
@@ -9,10 +9,11 @@ for(let i=0;i<16;i++){
     for(let j =0;j<16;j++){
 
         tableau[i][j] = document.createElement('div');
-        tableau[i][j].style.backgroundColor= 'pink';
+        
         tableau[i][j].style.width = '23px';
         tableau[i][j].style.height = '23px';
-        tableau[i][j].style.border= 'solid blue 1px'
+        tableau[i][j].style.border= 'solid skyblue 1px'
+        tableau[i][j].addEventListener('mouseover',() => tableau[i][j].setAttribute('class', 'getHover'))
         Container1.appendChild(tableau[i][j]);
 
     }
