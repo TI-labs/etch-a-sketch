a92121a9c7d1a0fdc8c16d64941cf5ccd1febe0e
Tidjani Stefel <tidjani.stefel@outlook.com>
2022-08-02 17:37:47 +0200

    Crée une nouvelle grille

--- logic.js
+++ logic.js
@@ -1,4 +1,15 @@
 
+function emptyContainer(container){
+
+    
+    
+
+}
+
+
+
+
+
 let Container1 = document.querySelector('.Container1');
 
 let tableau = [];
@@ -13,8 +24,45 @@ for(let i=0;i<16;i++){
         tableau[i][j].style.width = '23px';
         tableau[i][j].style.height = '23px';
         tableau[i][j].style.border= 'solid skyblue 1px'
-        tableau[i][j].addEventListener('mouseover',() => tableau[i][j].setAttribute('class', 'getHover'))
+        tableau[i][j].addEventListener('mouseover',() => tableau[i][j].setAttribute('class', 'getHover'));
         Container1.appendChild(tableau[i][j]);
 
     }
 }
+
+
+let btn = document.querySelector('.densityGrid');
+
+
+
+// btn.addEventListener('click',(e => {
+
+    
+    
+//     let numberOfBlock = Number(prompt('choose the density'));
+
+//     let blockWidth = Math.floor(400 / numberOfBlock);
+
+
+//     let strToShow = (blockWidth - 2).toString()  + 'px';
+//     console.log(strToShow);
+
+//     for(let i=0;i< numberOfBlock;i++){
+
+//         tableau[i]=[];
+//         for(let j =0;j< numberOfBlock;j++){
+    
+//             tableau[i][j] = document.createElement('div');
+            
+//             tableau[i][j].style.width = strToShow;
+//             tableau[i][j].style.height = strToShow;
+//             tableau[i][j].style.border= 'solid skyblue 1px'
+//             tableau[i][j].addEventListener('mouseover',() => tableau[i][j].setAttribute('class', 'getHover'))
+//             Container1.appendChild(tableau[i][j]);
+    
+//         }
+//     }
+
+
+
+// }))
