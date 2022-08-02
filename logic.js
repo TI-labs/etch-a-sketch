22a7be5c42aa7bd445ce17f07903b5a182ffc52c
Tidjani Stefel <tidjani.stefel@outlook.com>
2022-08-02 13:06:20 +0200

    Ajoutes des bordures sur chaque blocs

--- logic.js
+++ logic.js
@@ -10,8 +10,9 @@ for(let i=0;i<16;i++){
 
         tableau[i][j] = document.createElement('div');
         tableau[i][j].style.backgroundColor= 'pink';
-        tableau[i][j].style.width = '25px';
-        tableau[i][j].style.height = '25px';
+        tableau[i][j].style.width = '23px';
+        tableau[i][j].style.height = '23px';
+        tableau[i][j].style.border= 'solid blue 1px'
         Container1.appendChild(tableau[i][j]);
 
     }
